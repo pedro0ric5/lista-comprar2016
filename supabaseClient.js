0220a27083b6ee8d0cde5a8c64da28f8f8accc18
pedro0ric5 <pedro.ricardo.ribeiro@escola.pr.gov.br>
2026-03-05 10:39:49 -0300

    uhuhuh

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'https://ixugmhmmnvobcnzkqygu.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4dWdtaG1tbnZvYmNuemtxeWd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE1NzIsImV4cCI6MjA4NzY4NzU3Mn0.jRmwGRlHik1oActu6K07qpLRaLzFL0JNlnoRyYhAtDQ'
+
+  'https://dkuorkhqpffkzsdjlmrc.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrdW9ya2hxcGZma3pzZGpsbXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE5NTksImV4cCI6MjA4NzY4Nzk1OX0.uz7nd7kqavLQKcBOYorucifqfXZ3KPMkO5ce8trw5sA'
+  
 )
